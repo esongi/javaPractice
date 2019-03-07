c7dc3665140f6d4f2512f531414cbb1e85de1c8b
esongi <bazel29@naver.com>
2019-03-07 23:05:41 +0900

    java practice

--- everyJava/src/exam/test1/Test1.java
+++ everyJava/src/exam/test1/Test1.java
@@ -1,10 +1,16 @@
 package exam.test1;
 
+import java.util.Scanner;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		Test1 test1 = new Test1();
-		System.out.println(test1.solution(12));
+		System.out.print("값을 입력하세요:");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println("약수의 합: " + test1.solution(num));
+		sc.close();
 	}
 
 	public int solution(int n) {
@@ -12,8 +18,7 @@ public class Test1 {
 		for (int i = 1; i <= n / 2; i++) {
 			if (n % i == 0) {
 				answer += i;
-
-			} 
+			}
 		}
 		return answer + n;
 	}
--- everyJava/src/exam/test2/Test2.java
+++ everyJava/src/exam/test2/Test2.java
@@ -1,5 +1,26 @@
 package exam.test2;
 
+import java.util.Arrays;
+
 public class Test2 {
 
+	public static void main(String[] args) {
+		Test2 test = new Test2();
+		System.out.println(Arrays.toString(test.gcdlcm(2, 5)));
+	}
+
+	public int[] gcdlcm(int a, int b) {
+		int[] answer = new int[2];
+
+		answer[0] = gcd(a, b);
+		answer[1] = (a * b) / answer[0];
+		return answer;
+	}
+
+	public static int gcd(int p, int q) {
+		if (q == 0) {
+			return p;
+		}
+		return gcd(q, p % q);
+	}
 }
